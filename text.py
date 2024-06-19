177d3683d13eacebe1f09618217aa78ee244e18e
root <root@localhost>
2024-06-19 14:09:53 +0600

    auto publish 1718784592684

---
+++ text.py
@@ -0,0 +1,20 @@
+import datetime 
+
+print(datetime.datetime.now())
+
+print("\n")
+
+input_text = input("Enter text: ")
+
+print("n")
+
+repeat = int(input("Enter range: "))
+
+for i in range(repeat):
+     
+  print(input_text)
+  
+  print("\n")
+  
+print("exit.................joyriya akhtar")
+   
